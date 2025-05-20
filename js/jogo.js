1c832776466b4e59fc217f003d813e44c50fbd97
Emanoel Becker <emanoelbc01@gmail.com>
2025-05-20 09:11:17 -0300

    Minha versão estilizada

--- js/jogo.js
+++ js/jogo.js
@@ -30,7 +30,7 @@
       //percorremos todas as divs armazenadas
       for (i = 0; i < divis.length; i++) {
         //verificamos se sao as divs com ids 0 ou 1 ou 2
-        if (divis[i].id == 0 || divis[i].id == 1 || divis[i].id == 2) {
+        if (divis[i].id == 0 || divis[i].id == 1 || divis[i].id == 2 || divis[i].id == 3 || divis[i].id == 4) {
           //alteramos a classe css das divs 0, 1 e 2 (className)
           divis[i].className = "inicial";
         }
@@ -43,6 +43,12 @@
         //removemos a imagem do Smile
         imagem.remove();
       }
+
+      let imagemerrada = document.getElementById("imagemerrada");
+      if (imagemerrada != "") {
+        imagemerrada.remove();
+      }
+
     }
 
     //funçao que atualiza o placar
@@ -59,15 +65,26 @@
       //altera a classe CSS da <div> escolhida pelo jogador (className)
       obj.className = "acertou";
       //Criar uma constante img que armazena um novo objeto imagem com largura de 100px
-      const img = new Image(100);
+      const img = new Image(160);
       img.id = "imagem";
       //altera o atributo src (source) da imagem criada
-      img.src = "https://upload.wikimedia.org/wikipedia/commons/2/2e/Oxygen480-emotes-face-smile-big.svg";
+      img.src = "https://th.bing.com/th/id/OIP.f_WpA7aUC5dQUfMqZJ9bmAHaEX?rs=1&pid=ImgDetMain";
       //adiciona a imagem criada na div (obj) escolhida pelo jogador (appendChild)
       obj.appendChild(img);
     }
-
-    //Função que sorteia um número aleatório entre 0 e 2 e verifica se o jogador acertou
+    //função executada ao errar
+    function errou(obj) {
+      //altera a classe CSS da <div> escolhida pelo jogador (className)
+      obj.className = "errou";
+      const img = new Image(160);
+       //Criar uma constante img que armazena um novo objeto imagem com largura de 100px
+      img.id = "imagemerrada";
+       //altera o atributo src (source) da imagem criada
+      img.src = "https://th.bing.com/th/id/R.4b9796e8f45fa43e5bfcde974d0c1359?rik=Stw09tybHujgSA&riu=http%3a%2f%2f2.bp.blogspot.com%2f-EoN-SBEcb-0%2fUwMiFDAqYMI%2fAAAAAAAABsA%2ffdIAFSkYw48%2fs1600%2fGragas-League-of-Legends-Wallpaper-full-HD-5.jpg&ehk=VHcNKGYLBEd7otMvWeqBKW3ioXQnvTrrAhN2OwWS5Ds%3d&risl=&pid=ImgRaw&r=0";
+     //adiciona a imagem criada na div (obj) escolhida pelo jogador (appendChild) 
+      obj.appendChild(img);
+    }
+    //Função que sorteia um número aleatório entre 0 e 4 e verifica se o jogador acertou
     function verifica(obj) {
       //se jogar é verdadeiro
       if (jogar) {
@@ -75,15 +92,15 @@
         jogar = false;
         //incrementa as tentativas
         tentativas++;
-        //verifica se jogou 3 vezes
-        if (tentativas == 3) {
+        //verifica se jogou 5 vezes
+        if (tentativas == 5) {
           //oculta o botao joganovamente alterando a classe css (getElementById e className)
           btnJogarNovamente.className = 'invisivel';
           //mostra o botao reiniciar alterando a classe css (getElementById e className)
           btnReiniciar.className = 'visivel';
         }
         //a variável sorteado recebe um valor inteiro (Math.floor) aleatório (Math.random)
-        let sorteado = Math.floor(Math.random() * 3);
+        let sorteado = Math.floor(Math.random() * 4);
         //se o id da <div> escolhida pelo jogador for igual ao número sorteado
         if (obj.id == sorteado) {
           //chama a funçao acertou passando a div escolhida pelo jogador
@@ -92,7 +109,7 @@
           acertos++;
         } else {//se errou a tentativa
           //altera a classe da <div> escolhida pelo jogador para a classe errou
-          obj.className = "errou";
+          errou(obj)
           //armazena a div aonde Smile está escondido (getElementById)
           const objSorteado = document.getElementById(sorteado);
           //chama a funçao acertou para mostrar a div aonde está o Smile
